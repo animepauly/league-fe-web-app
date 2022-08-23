08a7cbbf0ef431f25046d6d80d62439568802182
Paul Borawski Jr <animepauly@mac.com>
2022-08-22 23:37:57 -0500

    setup amplify and added auth component

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "leaguefewebapp0dc0ce3f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
